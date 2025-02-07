57ec9ea58fdeef9dfb7f13bc956fc96ebc65bab1
Miguelki <63244249+miguelkix30@users.noreply.github.com>
2025-02-07 21:51:39 +0100

    Update config.js

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -6,21 +6,25 @@
 const pkg = require('../package.json');
 const nodeFetch = require("node-fetch");
 const convert = require('xml-js');
+
+import getKey from '../MKLib.js'
 let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
 
-let config = `${url}/launcher/config-launcher/config.json`;
+let config = `${url}/launcher/config-launcher/config.php`;
 let news = `${url}/launcher/news-launcher/news.json`;
+
 
 class Config {
     GetConfig() {
         return new Promise((resolve, reject) => {
-            nodeFetch(config).then(async config => {
+            let configUrl = `${config}?checksum=${getKey()}`;
+            nodeFetch(configUrl).then(async config => {
                 if (config.status === 200) return resolve(config.json());
                 else return reject({ error: { code: config.statusText, message: 'server not accessible' } });
             }).catch(error => {
                 return reject({ error });
-            })
-        })
+            });
+        });
     }
 
     async getInstanceList() {
